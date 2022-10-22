9b734ff365e45022841882f750d596c4e88172fc
unknown <derek.yipkwokchun@gmail.com>
2022-10-22 15:42:23 +0800

    product page finish ProductCanvas

--- public/component/ProductCanvas/ProductCanvas.js
+++ public/component/ProductCanvas/ProductCanvas.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import ProductCanvasStyle from '../../../styles/scss/ProductCanvas.module.scss'
 import ProductList from './ProductItems.json'
@@ -7,19 +7,19 @@ import Link from 'next/link'
 function ProductCanvas() {
 
     function ImgChanged(e) {
-        const target = e;
-        const img = target.getAttribute('productimg');
+        const productItem_wrapper = document.querySelectorAll(".productItem_wrapper")[e];
         const targetImg = document.querySelector('.productCanvas_inner img');
-        targetImg.src = img
+        targetImg.src = productItem_wrapper.getAttribute('productimg')
         console.log(targetImg.src);
+        
     }
 
     return (
     <div className={`productCanvas`} >
         <div className={`productCanvas_wrapper row d-flex`}>
-            <div className={`col-md-4 col-sm-5 bg-primary bg-opacity-25 p-5 shadow-lg`}>
-                <div className={`productCanvas_inner ${ProductCanvasStyle.productCanvas_inner}`}>
-                    <Image className={`h-75`} src={`/img/product/Green Capsules.png`} alt='productDemo' height={`1200`} width={`1000`} loading='eager'/>
+            <div className={`col-md-4 col-sm-5 bg-primary bg-opacity-25 p-5 shadow-lg d-flex justify-content-center align-items-center`}>
+                <div className={`productCanvas_inner d-flex justify-content-center align-items-center ${ProductCanvasStyle.productCanvas_inner}`}>
+                    <img src={`/img/product/Green Capsules.png`} alt='productDemo'></img>
                 </div>
             </div>
             <div className={`col-md-8 col-sm-5 bg-primary bg-opacity-50 mt-5 rounded-end p-0`}>
@@ -28,7 +28,7 @@ function ProductCanvas() {
                         return(
                              <div key={index} className={`productItem_wrapper w-100 d-flex flex-column align-items-end ${ProductCanvasStyle.productItem_wrapper}`} 
                                 style={{"--bk-color":`${items.color}`,"--productNum":`${index + 1}` }} 
-                                productimg={items.image_url} onMouseEnter={(e)=>{ImgChanged(e.target)}}
+                                productimg={items.image_url} onMouseEnter={(e)=>{ImgChanged(index)}}
                             >
                                 <p className="h1 mb-0 border-bottom text-end w-75 border-dark text-uppercase">
                                     {items.product}
