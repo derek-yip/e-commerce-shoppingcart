0b39e849d25258464fc4012c3c609023d83eb8a3
unknown <derek.yipkwokchun@gmail.com>
2022-10-22 23:30:31 +0800

    adjust CategoriesCanvas

--- public/component/CategoriesCanvas/Categories.js
+++ public/component/CategoriesCanvas/Categories.js
@@ -3,32 +3,41 @@ import CategoriesDetail from "./CategoriesDetail.json";
 import CategoriesCanvasStyle from '../../../styles/scss/CategoriesCanvas.module.scss'
 
 function Categories(props) {
-    console.log(props.layout);
   return (
     <div className={`Categories row ${CategoriesCanvasStyle.Categories}`}>
-        <div className={`col-${props.layout[0]} p-0 mh-100 h-100`}>
-            <div className={`d-flex flex-column justify-content-end align-items-end pe-5 border-bottom border-dark h-100 mb-auto`}>
-                <h1 className="h1 text-end text-uppercase fw-lighter">
-                    {props.left_title}
-                </h1>
-                <p className="h1 mb-0 text-uppercase fw-lighter">
-                    {props.left_content}
-                </p>
-            </div>
+        {
+            props.layout[0]!=0?
+                    <div className={`col-${props.layout[0]} p-0 mh-100`}>
+                        <div className={`d-flex flex-column justify-content-${props.text_position[0]} align-items-end pe-5 border-bottom border-dark h-100 mb-auto overflow-scroll`}>
+                            <h1 className="h1 text-end text-uppercase fw-lighter">
+                                {props.left_title}
+                            </h1>
+                            <p className="h1 mb-0 text-uppercase fw-lighter">
+                                {props.left_content}
+                            </p>
+                        </div>
+                    </div>
+            :''
+        }
+
+        <div className={`col-${props.layout[1]} p-0 h-100`} style={{ maxHeight:'100%', maxWidth:'100%', objectFit:'fill'}}>
+            <img src={props.img_url}/>
         </div>
-        <div className={`col-${props.layout[1]} p-0 `} style={{ maxHeight:'100%', maxWidth:'100%'}}>
-            <img src={`/img/product/categories/Food_01.jpeg`}/>
-        </div>
-        <div className={`col-${props.layout[2]} mh-100 p-0`}>
-            <div className={`border-bottom border-dark d-flex flex-column justify-content-start align-items-start`}>
-                <h1 className="h1 ps-5 text-end text-uppercase fw-lighter">
-                    {props.right_title}
-                </h1>
-                <p className="h1 ps-5 mb-0 text-uppercase fw-lighter">
-                    {props.right_content}
-                </p>
-            </div>
-        </div>
+
+        {
+            props.layout[2]!=0 ?
+                <div className={`col-${props.layout[2]} mh-100 p-0`}>
+                    <div className={`border-bottom border-dark d-flex flex-column justify-content-${props.text_position[1]} align-items-start`}>
+                        <h1 className="h1 ps-5 text-end text-uppercase fw-lighter">
+                            {props.right_title}
+                        </h1>
+                        <p className="h1 ps-5 mb-0 text-uppercase fw-lighter">
+                            {props.right_content}
+                        </p>
+                    </div>
+                </div>
+            :''
+        }
     </div>
   )
 }
--- public/component/CategoriesCanvas/CategoriesCanvas.js
+++ public/component/CategoriesCanvas/CategoriesCanvas.js
@@ -8,11 +8,31 @@ export default function CategoriesCanvas() {
         <div className={`CategoriesWrapper col-12 ${CategoriesCanvasStyle.CategoriesWrapper}`}>
             <Categories 
                 layout={[4,3,5]}
-                left_title={'Layout 2'}
+                text_position={['end','start']}
+                left_title={'Food'}
                 left_content={'Lorem ipsum dolor sit ametmet, consectetudolor sit amet, consectetur adipiscing dipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
-                right_title={'Layout 1'}
+                right_title={'Beverage'}
                 right_content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
+                img_url={'/img/product/categories/Food_01.jpg'}
+                convert_img={'/img/product/categories/Food_02.jpg'}
 
+            />
+            <Categories 
+                layout={[5,7,0]}
+                text_position={['center']}
+                left_title={'Food'}
+                left_content={'Lorem ipsum dolor sit ametmet, consectetudolor sit amet, consectetur adipiscing dipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
+                img_url={'/img/product/categories/Clothing_01.jpg'}
+                convert_img={'/img/product/categories/Clothing_02.jpg'}
+            />
+            
+            <Categories 
+                layout={[0,7,5]}
+                text_position={['','center']}
+                right_title={'Food'}
+                right_content={'Lorem ipsum dolor sit ametmet, consectetudolor sit amet, consectetur adipiscing dipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
+                img_url={'/img/product/categories/Clothing_01.jpg'}
+                convert_img={'/img/product/categories/Clothing_02.jpg'}
             />
         </div>
     </div>
