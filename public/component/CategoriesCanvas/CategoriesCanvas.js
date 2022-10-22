16729e3f66b43b698d014d613d6d87aef2c4da25
unknown <derek.yipkwokchun@gmail.com>
2022-10-22 23:48:07 +0800

    adjust CategoriesCanvas

--- public/component/CategoriesCanvas/CategoriesCanvas.js
+++ public/component/CategoriesCanvas/CategoriesCanvas.js
@@ -20,7 +20,7 @@ export default function CategoriesCanvas() {
             <Categories 
                 layout={[5,7,0]}
                 text_position={['center','','']}
-                left_title={'Food'}
+                left_title={'Clothing'}
                 left_content={'Lorem ipsum dolor sit ametmet, consectetudolor sit amet, consectetur adipiscing dipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
                 img_url={'/img/product/categories/Clothing_01.jpg'}
                 convert_img={'/img/product/categories/Clothing_02.jpg'}
@@ -29,10 +29,10 @@ export default function CategoriesCanvas() {
             <Categories 
                 layout={[0,7,5]}
                 text_position={['','','center']}
-                right_title={'Food'}
+                right_title={'Supplement'}
                 right_content={'Lorem ipsum dolor sit ametmet, consectetudolor sit amet, consectetur adipiscing dipiscing elit. Mauris accumsan justo ac nibh ullamcorper scelerisque. Aliquam vestibulum lectus ut accumsan sollicitudin. Aliquam ultrices varius urna. Integer pharetra eleifend eros, eu.'}
-                img_url={'/img/product/categories/Clothing_01.jpg'}
-                convert_img={'/img/product/categories/Clothing_02.jpg'}
+                img_url={'/img/product/categories/Supplement_01.jpg'}
+                convert_img={'/img/product/categories/Supplement_02.jpg'}
             />
         </div>
     </div>
