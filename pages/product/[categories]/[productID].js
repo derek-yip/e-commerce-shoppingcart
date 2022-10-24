962040278c2441a3e1e1c6b0cd86fa1e69c82c5b
Derek_yip <101183496+derek-yip@users.noreply.github.com>
2022-10-24 23:39:52 +0800

    Add serverSide in Category

--- pages/product/[categories]/[productID].js
+++ pages/product/[categories]/[productID].js
@@ -1,14 +1,41 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-function ProductItem() {
-  const router = useRouter()
+function ProductItem({product}) {
 
   return (
     <div>
-      <p>Product ID: {router.query.productId}</p>
+      <h1>{product.productName}</h1>
+      <h1>{product.categories}</h1>
+      <h1>{product.product_detail}</h1>
+      <h1>{product.price.originalPrice}</h1>
+      <h1>{product.nation}</h1>
     </div>
   )
 }
 
 export default ProductItem
+
+export async function getStaticProps(context){
+  const { params } = context
+  const response = await fetch(
+    `http://localhost:4000/ProductList/${params.id}`
+    )
+
+  const data = await response.json()
+
+  return{
+    props:{
+      product:data
+    }
+  }
+}
+
+export async function getStaticPaths(){
+  return{
+    paths:[{
+      params:{id:'63566dba91f9f9e98e50b5c2'}
+    }],
+    fallback:true
+  }
+}
--- pages/product/[categories]/index.js
+++ pages/product/[categories]/index.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import Image from 'next/image';
 
-function categories() {
+function categories({productList,categories}) {
+  console.log({productList});
   return (
-    <div>categories</div>
+    <div>
+    {
+      productList.map((product)=>{
+        return(
+          <div key={product.id}>
+              <h1>{product.productName}</h1>
+              <p>{product.product_detail}</p>
+              <h2>{product.categories}</h2>
+              <Image src={`/img/product/${product.img_url}`} width={500} height={500}/>
+          </div>
+        )
+      })
+    }
+    </div>
   )
 }
 
 export default categories
+
+export async function getServerSideProps(context){
+  const { params } = context
+  const {categories} = params
+  const response = await fetch(
+    `http://localhost:4000/ProductList?categories=${categories}`
+    )
+
+  const data = await response.json()
+
+  return{
+    props:{
+      productList:data,
+      categories
+    }
+  }
+}
