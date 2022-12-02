c346f1e732c5b604a5c381fc3d50df1ca5cec533
Derek_yip <101183496+derek-yip@users.noreply.github.com>
2022-12-02 20:34:42 +0800

    add adjust deploy

--- pages/product/[categories]/index.js
+++ pages/product/[categories]/index.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import ProductSliderBar from '../../../public/component/ProductSliderBar/ProductSLiderBar';
+import React from 'react';
+import ProductSliderBar from './component/ProductSliderBar/ProductSLiderBar';
 import Link from 'next/link';
 
-function categories({productList,categories}) {
+function categories({ productList, categories }) {
   return (
     <div className={`${categories}`}>
-      <ProductSliderBar List={productList}/>
-      
-      <button className={`w-80 h-100`}><Link href={'/product/categories'}> Back </Link></button>
-      
+      <ProductSliderBar List={productList} />
+
+      <button className={`w-80 h-100`}>
+        <Link href={'/product/categories'}> Back </Link>
+      </button>
     </div>
-  )
+  );
 }
 
-export default categories
+export default categories;
 
-export async function getServerSideProps(context){
-  const { params } = context
-  const {categories} = params
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { categories } = params;
   const response = await fetch(
     `http://localhost:4000/ProductList?categories=${categories}`
-    )
+  );
 
-  const data = await response.json()
+  const data = await response.json();
 
-  return{
-    props:{
-      productList:data,
-      categories
-    }
-  }
+  return {
+    props: {
+      productList: data,
+      categories,
+    },
+  };
 }
