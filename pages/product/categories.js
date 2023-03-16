59558f731766b0ad6d78af6d02a61e73202d8539
Derek_yip <101183496+derek-yip@users.noreply.github.com>
2023-03-17 00:24:14 +0800

    debug for deploy in json file

--- pages/product/categories.js
+++ pages/product/categories.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductSliderBar from '../../public/component/ProductSliderBar/ProductSliderBar';
-import productData from "../../data.json";
 
-function categories() {
+function categories({ data }) {
   return (
     <div>
-      <ProductSliderBar List={productData} />
+      <ProductSliderBar List={data} />
     </div>
   );
 }
 
 export default categories;
+
+export async function getServerSideProps() {
+  const response = await fetch(`https://derekyip.site/api/products`);
+
+  const data = await response.json();
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
